cb193d2013a32936ba38526b84531937afd8369b
Mukul-kudegave <mukulkudegave111@gmail.com>
2024-11-14 02:56:43 +0530

    jwt token and auth middlware added

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -3,7 +3,6 @@ import ErrorHandler from "../middlewares/errorMiddleware.js";
 import { User } from "../models/userSchema.js";
 import { generateTokens } from "../utils/jwtToken.js";
 
-
 export const PatientRegister = catchAsycnErrors(async (req, res, next) => {
   const {
     firstName,
@@ -26,7 +25,7 @@ export const PatientRegister = catchAsycnErrors(async (req, res, next) => {
     !gender ||
     !password ||
     !role
-  ){
+  ) {
     return next(new ErrorHandler("Please fill the form", 400));
   }
   let user = await User.findOne({ email });
@@ -44,33 +43,117 @@ export const PatientRegister = catchAsycnErrors(async (req, res, next) => {
     password,
     role,
   });
-  generateTokens(user, "User Register Successfully",200,res )
+  generateTokens(user, "User Register Successfully", 200, res);
 });
 
+export const login = catchAsycnErrors(async (req, res, next) => {
+  const { email, password, confirmPassword, role } = req.body;
+  if (!email || !password || !confirmPassword || !role) {
+    return next(new ErrorHandler("Please fill all the Details", 400));
+  }
+  if (password !== confirmPassword) {
+    return next(
+      new ErrorHandler("password and Confirmed password should be same!", 400)
+    );
+  }
+  const user = await User.findOne({ email }).select("+password");
+  if (!user) {
+    return next(new ErrorHandler("Invalid Password or Email!", 400));
+  }
 
-export const login = catchAsycnErrors(async(req,res,next)=>{
-    const {email, password, confirmPassword, role}=req.body;
-    if(!email || !password || !confirmPassword || !role){
-        return next(new ErrorHandler("Please fill all the Details",400));
-    }
-    if(password !== confirmPassword){
-        return next(new ErrorHandler("password and Confirmed password should be same!",400));
-        
-    }
-    const user = await User.findOne({email}).select("+password")
-    if(!user){
-        return next(new ErrorHandler("Invalid Password or Email!",400));
-    }
+  const isPasswordMatched = await user.comparePassword(password);
+  if (!isPasswordMatched) {
+    return next(new ErrorHandler("Invalid Password or Email!", 400));
+  }
 
-    const isPasswordMatched = await user.comparePassword(password);
-    if(!isPasswordMatched){
-        return next(new ErrorHandler("Invalid Password or Email!",400));
-    }
+  if (role !== user.role) {
+    return next(new ErrorHandler("User with this role not found!", 400));
+  }
+  generateTokens(user, "User Register Successfully", 200, res);
+});
 
-    if(role !== user.role){
-        return next(new ErrorHandler("User with this role not found!",400));
-    }
-    generateTokens(user, "User Register Successfully",200,res )
-})
+export const addNewAdmin = catchAsycnErrors(async (req, res, next) => {
+  const { firstName, lastName, email, phone, Aadhar, dob, gender, password } =
+    req.body;
+  if (
+    !firstName ||
+    !lastName ||
+    !email ||
+    !phone ||
+    !Aadhar ||
+    !dob ||
+    !gender ||
+    !password
+  ) {
+    return next(new ErrorHandler("Please fill the form", 400));
+  }
+  const isRegistered = await User.findOne({ email });
+  if (isRegistered) {
+    return next(
+      new ErrorHandler(
+        `${isRegistered.role} with this email is Already Exists!`,
+        400
+      )
+    );
+  }
+  const admin = await User.create({
+    firstName,
+    lastName,
+    email,
+    phone,
+    Aadhar,
+    dob,
+    gender,
+    password,
+    role: "Admin",
+  });
+  res.status(200).json({
+    success: true,
+    message: "New Admin Registered",
+  });
+});
+
+export const getAllDoctors = catchAsycnErrors(async (req, res, next) => {
+  const doctors = await User.find({ role: "Doctor" });
+  res.status(200).json({
+    success: true,
+    doctors,
+  });
+});
+
+export const getUserDetails = catchAsycnErrors(async (req, res, next) => {
+  const user = req.user;
+  res.status(200).json({
+    success: true,
+    user,
+  });
+});
+
+export const logoutAdmin = catchAsycnErrors(async (req, res, next) => {
+  res
+    .status(200)
+    .cookie("adminToken", "", {
+      httpOnly: true,
+      expires: new Date(Date.now()),
+    })
+    .json({
+      success: true,
+      message: "Admin Log out Successfully",
+    });
+});
+
+export const logoutPatient = catchAsycnErrors(async (req, res, next) => {
+  res
+    .status(200)
+    .cookie("patientToken", "", {
+      httpOnly: true,
+      expires: new Date(Date.now()),
+    })
+    .json({
+      success: true,
+      message: "User Log out Successfully",
+    });
+});
+
 
 // export const 
--- backend/router/userRouter.js
+++ backend/router/userRouter.js
@@ -1,9 +1,16 @@
 import express from "express";
-import { login, PatientRegister } from '../controller/userController.js';
+import { addNewAdmin, getAllDoctors, getUserDetails, login, logoutAdmin, logoutPatient, PatientRegister } from '../controller/userController.js';
+import { isAdminAuthenticated, isPatientAuthenticated } from "../middlewares/auth.js";
 
 const router = express.Router();
 
 router.post("/patient/register",PatientRegister)
 router.post("/login",login)
+router.post("/admin/addnew",isAdminAuthenticated ,addNewAdmin)
+router.get("/doctors",getAllDoctors);
+router.get("/admin/me",isAdminAuthenticated, getUserDetails)
+router.get("/patient/me", isPatientAuthenticated, getUserDetails)
+router.get("/admin/logout", isAdminAuthenticated, logoutAdmin)
+router.get("/patient/logout", isPatientAuthenticated, logoutPatient)
 
 export default router;
