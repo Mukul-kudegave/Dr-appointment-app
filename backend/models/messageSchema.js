9211a36e06aa477e2f38fe23e411e806fada2aba
Mukul-kudegave <mukulkudegave111@gmail.com>
2024-11-13 22:37:25 +0530

    MessageSchema created

---
+++ backend/models/messageSchema.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+import validator from "validator";
+
+const messageSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+    minLength: [3, "First name contain at least 3 character"],
+  },
+  lasttName: {
+    type: String,
+    required: true,
+    minLength: [3, "Last name contain at least 3 character"],
+  },
+  email: {
+    type: String,
+    required: true,
+    validator: [validator.isEmail, "Please provide valid email"],
+  },
+  phone: {
+    type: String,
+    required: true,
+    minLength: [10, "Phone no. must contain at least 10 digit"],
+    maxLength: [10, "Phone no. must contain at least 10 digit"],
+  },
+  message: {
+    type: String,
+    required: true,
+    minLength: [10, "Message must contain at least 10 characters"],
+  },
+});
+
+export const Message = mongoose.model("Message", messageSchema)
