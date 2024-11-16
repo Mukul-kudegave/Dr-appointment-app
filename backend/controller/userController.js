8711019a3ee5734d62e77947e458362b8d34fd2f
Mukul-kudegave <mukulkudegave111@gmail.com>
2024-11-16 16:55:15 +0530

    Error Corrected

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -136,6 +136,8 @@ export const logoutAdmin = catchAsycnErrors(async (req, res, next) => {
     .cookie("adminToken", "", {
       httpOnly: true,
       expires: new Date(Date.now()),
+      secure:true,
+      sameSite:"None",
     })
     .json({
       success: true,
@@ -149,6 +151,8 @@ export const logoutPatient = catchAsycnErrors(async (req, res, next) => {
     .cookie("patientToken", "", {
       httpOnly: true,
       expires: new Date(Date.now()),
+      secure:true,
+      sameSite:"None",
     })
     .json({
       success: true,
