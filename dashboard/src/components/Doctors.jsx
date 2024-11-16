be3e84d2db6fd4d18e1239bd262a5f7968954e8f
Mukul-kudegave <mukulkudegave111@gmail.com>
2024-11-16 16:38:38 +0530

    Error Corrected

--- dashboard/src/components/Doctors.jsx
+++ dashboard/src/components/Doctors.jsx
@@ -7,6 +7,7 @@ import { Navigate } from "react-router-dom";
 const Doctors = () => {
   const [doctors, setDoctors] = useState([]);
   const { isAuthenticated } = useContext(Context);
+
   useEffect(() => {
     const fetchDoctors = async () => {
       try {
@@ -25,42 +26,41 @@ const Doctors = () => {
   if (!isAuthenticated) {
     return <Navigate to={"/login"} />;
   }
+
   return (
     <section className="page doctors">
       <h1>DOCTORS</h1>
       <div className="banner">
         {doctors && doctors.length > 0 ? (
-          doctors.map((element) => {
-            return (
-              <div className="card">
-                <img
-                  src={element.docAvatar && element.docAvatar.url}
-                  alt="doctor avatar"
-                />
-                <h4>Dr.{`${element.firstName} ${element.lastName}`}</h4>
-                <div className="details">
-                  <p>
-                    Email: <span>{element.email}</span>
-                  </p>
-                  <p>
-                    Phone: <span>{element.phone}</span>
-                  </p>
-                  <p>
-                    DOB: <span>{element.dob.substring(0, 10)}</span>
-                  </p>
-                  <p>
-                    Department: <span>{element.doctorDepartment}</span>
-                  </p>
-                  <p>
-                    Aadhar: <span>{element.Aadhar}</span>
-                  </p>
-                  <p>
-                    Gender: <span>{element.gender}</span>
-                  </p>
-                </div>
+          doctors.map((element, index) => (
+            <div className="card" key={element._id || index}>
+              <img
+                src={element.docAvatar && element.docAvatar.url}
+                alt="doctor avatar"
+              />
+              <h4>Dr.{`${element.firstName} ${element.lastName}`}</h4>
+              <div className="details">
+                <p>
+                  Email: <span>{element.email}</span>
+                </p>
+                <p>
+                  Phone: <span>{element.phone}</span>
+                </p>
+                <p>
+                  DOB: <span>{element.dob.substring(0, 10)}</span>
+                </p>
+                <p>
+                  Department: <span>{element.doctorDepartment}</span>
+                </p>
+                <p>
+                  Aadhar: <span>{element.Aadhar}</span>
+                </p>
+                <p>
+                  Gender: <span>{element.gender}</span>
+                </p>
               </div>
-            );
-          })
+            </div>
+          ))
         ) : (
           <h1>No Registered Doctors Found!</h1>
         )}
